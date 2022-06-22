1c51a0bf12411060038bc2e28958dc5103e85012
Mohamad Romli Firdaus Kamarula <ramaera321@gmail.com>
2022-06-22 22:54:35 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import uuid from 'uuid'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state={
+			items: [],
+			itemsToShow: "all",
+			id: uuid(),
+			item: '',
+			editItem: false,
+		}
+	}
+
+	handleChange = event => {
+		this.setState({
+			item: event.target.value
+		})
+	}
+
+	handleSubmit = event => {
+		event.preventDefault()
+		
+		const newItem = {
+			id: this.state.id,
+			title: this.state.item,
+			completed: false
+		}
+		
+		const updatedItems = [...this.state.items, newItem]
+
+		if (this.state.item.length > 0) {
+			this.setState({
+				items: updatedItems,
+				id: uuid(),
+				item: '',
+				editItem: false
+			})
+		}
+	}
+
+	handleDelete = id => {
+		const filteredItems = this.state.items.filter(item => item.id !== id)
+
+		this.setState({
+			items: filteredItems
+		})
+	}
+
+	handleEdit = id => {
+		const filteredItems = this.state.items.filter(item => item.id !== id)
+
+		const selectedItem = this.state.items.find(item => item.id === id)
+
+		this.setState({
+			items: filteredItems,
+			id: id,
+			item: selectedItem.title,
+			editItem: true
+		})
+	}
+
+	clearList = () => {
+		this.setState({
+			items: []
+		})
+	}
+
+	render() {
+		let items = []
+
+		if (this.state.itemsToShow === "all") {
+			items = this.state.items;
+		} else if (this.state.itemsToShow === "todo") {
+			items = this.state.items.filter(item => !item.completed);
+		} else if (this.state.itemsToShow === "done") {
+			items = this.state.items.filter(item => item.completed);			
+		}
+
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-10 col-md-8 mx-auto mt-4">
+						<h3 className="text-capitalize text-center">TodoInput</h3>
+						<TodoInput
+							item={this.state.item}
+							handleChange={this.handleChange}
+							handleSubmit={this.handleSubmit}
+						/>
+						<TodoList
+							items={items}
+							// filterDoneTasks={this.filterDoneTasks}
+							clearList={this.clearList}
+							handleDelete={this.handleDelete}
+							handleEdit={this.handleEdit}
+							// handleDoneTask={this.handleDoneTask}
+							// handleDeleteDoneTasks={this.handleDeleteDoneTasks}
+							updateTodosToShow={this.updateTodosToShow}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
